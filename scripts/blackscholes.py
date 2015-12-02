22e605a4d9cec3a26ecb707a3b10b0f7df99c3a0
borriquear <borriquear@gmail.com>
2015-12-01 23:56:25 -0500

    modifs in code for bs model

--- scripts/blackscholes.py
+++ scripts/blackscholes.py
@@ -2,8 +2,13 @@
 from sympy import *
 from scipy import stats
 import math
+import os.path
 import numpy as np
+import matplotlib
+import matplotlib.artist as artists
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import savefig
+
 def black_scholes (cp, s, k, t, v, rf, div):
         """ Price an option using the Black-Scholes model.
         s: initial stock price
@@ -20,114 +25,109 @@ def black_scholes (cp, s, k, t, v, rf, div):
 
         optprice = (cp*s*math.exp(-div*t)*stats.norm.cdf(cp*d1)) - (cp*k*math.exp(-rf*t)*stats.norm.cdf(cp*d2))
         return optprice
-	print " Print"
-	print optprice
+    #print " Print"
+    #print optprice
 
-def black_scholes_hedonic (k, final_time, v):
-        """ Price an option using the Black-Scholes model.
-        s: pred_pleasure -- initial stock price
-        pred_error --inverse of  pred_pleasure 
+def black_scholes_hedonic (p0, k, final_time, v, r):
+    """ Price an option using the Black-Scholes model.
+        p0: initial pred_pleasure -- initial stock price
         k: boredom constant --strike price
-        t: expiration time
+        final_time: expiration time
         v: sigma -- volatility
-	#meas_error = 
-        """
-	t = np.arange(0.0,final_time,0.1)
-	# calculate P (Prediction) component
-	#mean and standard deviation of the measurement error
-	#prediction error and mesurement error are different, meas_error can not be eliminated, prediction error you can if perfect predictions
-	mu_error = 0
-	sigma_error = 0.01   
-	meas_error = np.random.normal(mu_error, sigma_error, len(t))
-	pred_error = np.exp(-t/v + meas_error)
-	#pred_error_prime = pred_error.diff(t)
-	pred_error_prime = np.exp(-t/v) 
-	#t = Symbol('t') 
-	#pred_error = np.exp(-t/v) 
-	pred_pleasure = 1/pred_error
-	s = pred_pleasure
-	rf = pred_error_prime
-        d1 = (np.log(s/k)+(rf+0.5*np.power(v,2)))/(v)
-        d2 = d1 - v
-	print "Prediction error is:"
-	print pred_error
-	print "Prediction pleasure is:"
-	print pred_pleasure
-	pred_error_price = []
-	pred_pleasure_price = []
-	d2list = []
-	d1list = []
-	d2list_boredom = []
-	for i in xrange(len(d1)):
-		d2_element = stats.norm.cdf(d2[i])
-		d1_element = stats.norm.cdf(d1[i])
-		#x = np.asarray(pred_error[i])*np.asarray(d2_element) 
-		#y = np.asarray(pred_pleasure[i])*np.asarray(d2_element) 
-		x = np.asarray(pred_error[i])*np.asarray(d2_element) 
-		y = np.asarray(pred_pleasure[i])*np.asarray(d1_element) 
-		pred_error_price.append(x)
-		pred_pleasure_price.append(x)
-		d2_boredom_element = 1 - d2_element
-		d2list.append(d2_element)
-		d1list.append(d1_element)
-		d2list_boredom.append(d2_boredom_element)
-	print "d1 prob of P"
-	print d1list
-	print "d2 prob of  B"
-	print d2list
-	print "pred_error"
-	print pred_error
-	print "error price"
-	print pred_error_price
-	print "pred pleasure"
-	print pred_pleasure
-	print "pleasure price"
-	print pred_pleasure_price
-	error_line, = plt.plot(t,pred_error, 'r', label = "e")
-	error_legend = plt.legend(handles=[error_line], loc=1)
-	ax = plt.gca().add_artist(error_legend)
-	s_line, = plt.plot(t,pred_pleasure, 'g', label = "s")
-	s_legend = plt.legend(handles=[s_line], loc=2)
-	ax = plt.gca().add_artist(s_legend)
-	d2_line, = plt.plot(t,d2list, 'r--', label = "p(d2)")
-	d2_legend = plt.legend(handles=[d2_line],loc=3)
-	ax = plt.gca().add_artist(d2_legend)
-	pleasure_line, = plt.plot(t,pred_pleasure_price, 'g--', label = "(s*d2)")
-	pleasure_legend = plt.legend(handles=[pleasure_line],loc=4)
-	ax = plt.gca().add_artist(pleasure_legend)
-	plt.suptitle(' V = P - B')
-	plt.xlabel('time')
-	plt.ylabel('prediction/pleasure error')
-	k_list = [k]*len(d2list)	
-	k_fact_list = k/rf
-	boredom_line, = plt.plot(t,k_fact_list, 'y')
-	boredom_legend = plt.legend(handles=[boredom_line], loc=5)	
-	ax = plt.gca().add_artist(boredom_legend)
-	k_list = [k]*len(d2list)	
-	# calculate B (Boredom) component
-	boredom = np.maximum(k_fact_list, k_list) 
-	#boredom_fact = boredom*d2list_boredom
-	boredom_fact = boredom*d2list
-	# Subjective value to maximize
-	subjective_value = pred_pleasure_price - boredom_fact
-	print " boredom"
-	print boredom
-	print " boredom fact"
-	print boredom_fact
-	print " subjecive value"
-	print subjective_value
-	boredom_bound_line, = plt.plot(t,boredom, 'c', label = "B")
-	boredom_bound_legend = plt.legend(handles=[boredom_bound_line], loc=6)
-	ax = plt.gca().add_artist(boredom_bound_legend)
-	boredom_fact_line, = plt.plot(t,boredom_fact, 'c--', label = "B*(1-d2)")
-	boredom_fact_legend = plt.legend(handles=[boredom_fact_line],loc =7)
-	ax = plt.gca().add_artist(boredom_fact_legend)
-	v_line, = plt.plot(t,subjective_value, linewidth= 3 , c="black",label = "V") 
-	v_legend = plt.legend(handles=[v_line],loc=8)
-	ax = plt.gca().add_artist(v_legend)
-	plt.show()
- 
-k=10
-final_time = 100
-v= 2
-black_scholes_hedonic (k, final_time, v)
+        r: pedictability ration of the external world. k(w)/w = 1 totally random world, r = 0 totally pedictable
+    """    
+    #time vector    
+    t_slots = 0.001
+    t = np.arange(0.0,final_time,t_slots)
+    mu_error = 0.0
+    sigma_error = v
+    #meas_error = np.random.normal(mu_error, sigma_error, len(t))
+    #pred_error = np.exp(-t/v + meas_error)
+    #pred_error_prime = np.exp(-t/v) #pred_error_prime = pred_error.diff(t)
+    #mean of the logarithm of predictio error
+    std_pred_error = sigma_error*np.sqrt(final_time - t)
+    std_pred_pleas = std_pred_error
+    mean_pred_error = (mu_error - np.power(sigma_error,2)*0.5)*(final_time - t)
+    mean_pred_pleas = (-mu_error + np.power(sigma_error,2)*0.5)*(final_time - t)
+    #monte carlo sample of the log prediction pleasure
+    mc_pred_pleas_log = np.random.normal(np.log(p0) + mean_pred_pleas, std_pred_pleas)
+    mc_pred_pleas = np.log(p0) + np.exp(mean_pred_pleas)
+    mc_pred_pleas_t =  np.exp(mc_pred_pleas_log)
+    boredom_pain= k*np.exp(-r*(final_time - t))
+    print "std_pred_pleas"
+    print std_pred_pleas
+    print " mc_pred_pleas_log .."
+    print mc_pred_pleas_log
+    print " mc_pred_pleas .."
+    print mc_pred_pleas
+    print "mc_pred_pleas_t"
+    print mc_pred_pleas_t
+    
+    #return
+    
+    #d1 = (np.log(mc_pred_pleas/k) + (r + 0.5*np.power(v,2))*(final_time - t))/(v*np.sqrt(final_time - t))
+    #d2 = (np.log(k/mc_pred_pleas) + (r - 0.5*np.power(v,2))*(final_time - t))/(v*np.sqrt(final_time - t))
+    #d1 = (np.log(mc_pred_pleas/k) - (r + 0.5*np.power(v,2))*(final_time - t))/(v*np.sqrt(final_time - t))
+    #d2 = 1 - d1
+    d2 = (np.log(k/mc_pred_pleas) + (-r + 0.5*np.power(v,2))*(final_time - t))/(v*np.sqrt(final_time - t))
+    #d1 = 1 - d2    
+    pred_pleasure_price = []    
+    boredom_pain_price = []
+    subjective_experience_price = []
+    d1list = [] 
+    d2list = []
+    for i in xrange(len(d2)):
+        #d1_element = stats.norm.cdf(d1[i])
+        d2_element = stats.norm.cdf(d2[i])
+        d1_element = 1 - d2_element
+        d1list.append(d1_element)
+        d2list.append(d2_element)
+        pred_pleas_w = np.asarray(mc_pred_pleas[i])*np.asarray(d1_element)
+        boredom_w = (np.asarray(boredom_pain[i]))*np.asarray(d2_element)  
+        subjective_experience_w = pred_pleas_w - boredom_w
+        pred_pleasure_price.append(pred_pleas_w)
+        boredom_pain_price.append(boredom_w)   
+        subjective_experience_price.append(subjective_experience_w)
+    print "d1 prob of P"
+    print d1list
+    print "d2 prob of  B"
+    print d2list
+    print "pred_pleasure_price"
+    print pred_pleasure_price
+    print "boredom_pain_price"
+    print boredom_pain_price
+    print "subjective_experience_price"
+    print subjective_experience_price
+    #Charts
+    #plt.plot(t,pred_pleasure_price,'g--', label = "p")
+    #plt.plot(t,boredom_pain_price,'y', label = "b") 
+    #plt.plot(t,subjective_experience_price,'b', label = "v")
+    fig=plt.figure(1)  
+    ax = plt.subplot(111)
+    ax.plot(t, d1list, 'g--', label = "$N(d_{1})$")
+    ax.plot(t, d2list, 'r--', label = "$N(d_{2})$")
+    ax.plot(t, pred_pleasure_price, 'g', label = "$Prediction (p)$")
+    ax.plot(t, boredom_pain_price, 'r', label = "$Boredom (b)$")
+    ax.plot(t, subjective_experience_price, 'b', label = "$Experience (v)$")
+    #ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    lgd = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+          fancybox=True, shadow=True, ncol=3)
+    plt.title('$r = %.1f $' %r)
+    plt.title('$p_0/k = %.2f$' %ratiopk, loc='left')
+    plt.title('$v = p - b$', loc='right')
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$value$')   
+    plt.show()
+    figures_path = 'c:/workspace/github/figures'
+    base_filename = 'r0-u0v1-p1k1'
+    filename_suffix = 'png'
+    dest_file = os.path.join(figures_path, base_filename + "." + filename_suffix)
+    fig.savefig(dest_file, dpi=600, format='png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+
+final_time = 1
+v= 1.0
+r = 0.0
+p0 = 1.0
+k = 1.0
+ratiopk = p0/k
+black_scholes_hedonic (p0, k, final_time, v, r)
